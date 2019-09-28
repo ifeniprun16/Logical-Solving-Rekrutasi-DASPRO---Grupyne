b4b7efdddd020064cbf4bf4d4fb8b4ec318e57e7
ifeniprun16 <45282204+ifeniprun16@users.noreply.github.com>
2019-09-28 21:21:49 +0800

    Update nomer1.java

--- LogicalSolving/nomer1.java
+++ LogicalSolving/nomer1.java
@@ -1,41 +1,20 @@
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 public class nomer1 {
     public static void main(String[] args) {
-        LinkedList<Integer> list1 = new LinkedList();
-        LinkedList<Integer> list2 = new LinkedList();
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list1 = new ArrayList();
+        ArrayList<Integer> list2 = new ArrayList();
+        ArrayList<Integer> sama = new ArrayList();
         System.out.println("Input Faktor Persekutuan 2 Bilangan");
-        int angka1, angka2;
         System.out.print("Angka Pertama : ");
-        angka1 = in.nextInt();
+        int angka1 = input.nextInt();
         System.out.print("Angka Kedua : ");
-        angka2 = in.nextInt();
-        int j = 0;
-        System.out.print("Faktor dari " + angka1 + " = ");
-        for (int i = 1; i <= angka1; i++) {
-            if (angka1 % i == 0) {
-                list1.add(i);
-                System.out.print(list1.get(j) + " ");
-                j++;
-            }
-        }
-        System.out.println("");
-        System.out.print("Faktor dari " + angka2 + " = ");
-        j = 0;
-        for (int i = 1; i <= angka2; i++) {
-            if (angka2 % i == 0) {
-                list2.add(i);
-                System.out.print(list2.get(j) + " ");
-                j++;
-            }
-        }
-        System.out.println("");
-        System.out.print("Faktor dari " + angka1 + " dan " + angka2 + " = ");
-        for (int i = 0; i < list1.size(); i++) {
-            if (list2.contains(list1.get(i))) {
-                System.out.print(list1.get(i) + " ");
-            }
-        }
+        int angka2 = input.nextInt();
+        for (int i = 1; i <=angka1; i++) if(angka1%i==0) list1.add(i);
+        for (int i = 1; i <=angka2; i++) if(angka2%i==0) list2.add(i);
+        for (int i =0;i<list1.size();i++) if(list2.contains(list1.get(i))) sama.add(list1.get(i));
+        System.out.println("Faktor dari " + angka1 + " = "+list1);
+        System.out.println("Faktor dari " + angka2 + " = "+list2);
+        System.out.print("Faktor dari " + angka1 + " dan " + angka2 + " = "+sama);
     }
 }
